171598d7de864e536430e3a62d35fa0339b31490
96234 <962349367@qq.com>
2021-10-20 23:26:13 +0800

    Initial commit

---
+++ src/main/java/com/mm/qbot/QbotApplication.java
@@ -0,0 +1,13 @@
+package com.mm.qbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mm/qbot/QbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.qbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
