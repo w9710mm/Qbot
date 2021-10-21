8c7985c50fb92a0eaea61981fbdb99da1cf6b1ee
96234 <962349367@qq.com>
2021-10-21 13:41:20 +0800

    test

---
+++ src/main/java/com/mm/qbot/utlis/QrcodeUtils.java
@@ -0,0 +1,30 @@
+package com.mm.qbot.utlis;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.nio.file.Path;
+
+/**
+ * @author WWM
+ * @version V0.0.1
+ * @Package com.mm.qbot.utlis
+ * @Description: 生成二维码的工具
+ * @date 2021/10/21 12:16
+ */
+public class QrcodeUtils {
+    public static void createQrcode(String content, Path file) throws Exception {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bm = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, 100, 100);
+            System.out.print(bm.toString());
+            MatrixToImageWriter.writeToPath(bm, "png", file);
+        } catch (WriterException e) {
+            e.getStackTrace();
+        }
+    }
+
+}
--- src/test/java/com/mm/qbot/QbotApplicationTests.java
+++ src/test/java/com/mm/qbot/QbotApplicationTests.java
@@ -1,13 +1,50 @@
 package com.mm.qbot;
 
+import com.alibaba.fastjson.JSONObject;
+import com.mm.qbot.utlis.QrcodeUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.nio.file.Path;
+import java.util.Map;
+
 class QbotApplicationTests {
-
+    private  RestTemplate restTemplate=new RestTemplate();
     @Test
-    void contextLoads() {
+    void bilibili() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("referer","https://www.bilibili.com/");
+        headers.add("user-agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(headers);
+
+        String islogin="https://account.bilibili.com/home/userInfo";
+        String url="https://passport.bilibili.com/qrcode/getLoginUrl";
+        ResponseEntity<String> QrresponseEntity = restTemplate.getForEntity(url,httpEntity ,String.class);
+
+
+        String user = QrresponseEntity.getBody();
+        assert user != null;
+
+       JSONObject json = JSONObject.parseObject(user);
+        json.getInteger("coded");
+        String data = json.getJSONObject("data").getString("url");
+        try {
+            QrcodeUtils.createQrcode(data, Path.of("1.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ResponseEntity<String> isloginRe = restTemplate.getForEntity(islogin, String.class);
+        System.out.println(isloginRe);
+
+
+//        System.out.print(json.toJSONString());
+
+
     }
 
 }
