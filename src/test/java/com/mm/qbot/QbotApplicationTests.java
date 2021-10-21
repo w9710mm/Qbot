92182ad5bf2b6588e0fe6f05943b84d16c35e1c6
96234 <962349367@qq.com>
2021-10-21 21:36:30 +0800

    test

--- src/test/java/com/mm/qbot/QbotApplicationTests.java
+++ src/test/java/com/mm/qbot/QbotApplicationTests.java
@@ -4,15 +4,14 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.mm.qbot.strategy.BilibiliStrategy;
 import com.mm.qbot.utlis.BilibiliApi;
+import com.mm.qbot.utlis.QrcodeUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -88,5 +87,39 @@ class QbotApplicationTests {
         headers.add("referer","https://search.bilibili.com/upuser?keyword=%E7%AD%89%E7%AD%89&from_source=web_search");
         ResponseEntity<String> res=restTemplate.exchange(url, HttpMethod.GET,httpEntity,String.class);
         System.out.println((res.getBody()));
+    }
+
+
+    @Test
+    void bilibili() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("referer","https://www.bilibili.com/");
+        headers.add("user-agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(headers);
+
+        String islogin="https://account.bilibili.com/home/userInfo";
+        String url="https://passport.bilibili.com/qrcode/getLoginUrl";
+        ResponseEntity<String> QrresponseEntity = restTemplate.getForEntity(url,httpEntity ,String.class);
+
+
+        String user = QrresponseEntity.getBody();
+        assert user != null;
+
+        JSONObject json = JSONObject.parseObject(user);
+        json.getInteger("coded");
+        String data = json.getJSONObject("data").getString("url");
+        try {
+            QrcodeUtils.createQrcode(data, Path.of("1.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ResponseEntity<String> isloginRe = restTemplate.getForEntity(islogin, String.class);
+        System.out.println(isloginRe);
+
+
+//        System.out.print(json.toJSONString());
+
+
     }
 }
